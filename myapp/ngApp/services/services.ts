6c1e54b40336e84c6936a850abd025e177f8b37f
Angelina James <scrog58@hotmail.com>
2017-05-02 11:05:23 -0700

    Message

--- myapp/ngApp/services/services.ts
+++ myapp/ngApp/services/services.ts
@@ -1,3 +1,26 @@
 namespace myapp.Services {
 
+  class WeightService {
+    public WEIGTH_RESOURCE = this.$resource('api/v1/weight');
+
+    constructor(private $resource) {}
+
+    public list() {
+      return this.WEIGTH_RESOURCE.query();
+    }
+
+    public save(weight) {
+      return this.WEIGTH_RESOURCE.save(weight).$promise;
+    }
+
+    public get(id){
+      return this.WEIGTH_RESOURCE.get({id: id});
+    }
+
+    public delete(id) {
+      return this.WEIGTH_RESOURCE.delete(id).$promise;
+    }
+
+  }
+  angular.module('myapp').service('weightService', WeightService);
     }
